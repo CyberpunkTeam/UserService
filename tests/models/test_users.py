3a83289cc7e81106b52159eda53ba71e3329acd3
matfonseca <mfonseca@fi.uba.ar>
2022-11-30 19:38:27 -0300

    fix tests

--- tests/models/test_users.py
+++ tests/models/test_users.py
@@ -2,7 +2,9 @@ from app.models.users import Users
 
 
 def test_create_user():
-    user = Users(name="Martin", lastname="Diaz", email="mdiaz@gmail.com")
+    user = Users(
+        name="Martin", lastname="Diaz", email="mdiaz@gmail.com", location="Buenos Aires"
+    )
     assert user.name == "Martin"
     assert user.lastname == "Diaz"
     assert user.email == "mdiaz@gmail.com"
