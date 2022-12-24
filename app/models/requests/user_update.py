b84f99e55119c54d1ddb2b2960b3ecaf83ef3a8b
matfonseca <mfonseca@fi.uba.ar>
2022-12-24 00:54:46 -0300

    add created_date and updated_date

--- app/models/requests/user_update.py
+++ app/models/requests/user_update.py
@@ -10,6 +10,8 @@ class UserUpdate(BaseModel):
     location: Optional[str] = ""
     profile_image: Optional[str] = ""
     cover_image: Optional[str] = ""
+    created_date: Optional[str] = ""
+    updated_date: Optional[str] = ""
 
     def to_json(self):
         return loads(self.json(exclude_defaults=True))
