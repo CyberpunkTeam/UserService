7d37bc01664d045f90e39ab2299813cc419a7f49
matifonseca <matias.fonseca@mercadolibre.com>
2023-02-13 19:35:39 -0300

    test locations

---
+++ tests/controllers/test_locations_controller.py
@@ -0,0 +1,29 @@
+from unittest.mock import Mock
+
+import mongomock
+import pytest
+from fastapi import HTTPException
+
+from app import config
+from app.controllers.location_controller import LocationController
+from app.controllers.user_controller import UserController
+from app.models.locations import Locations
+from app.models.users import Users
+from app.repositories.locations_repository import LocationsRepository
+
+
+@mongomock.patch(servers=(("server.example.com", 27017),))
+def test_get_cities_and_country_start_with_bue():
+    location_a = Locations(city="Buenos Aires", country="Argentina")
+    location_b = Locations(city="La paz", country="Bolivia")
+
+    url = config.DATABASE_URL
+    db_name = config.DATABASE_NAME
+    repository = LocationsRepository(url, db_name)
+
+    ok = repository.insert_many([location_a, location_b])
+    assert ok
+
+    result = LocationController.search(repository, "bue")
+
+    assert len(result) == 1
